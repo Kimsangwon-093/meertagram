5b083882fa8e9fba422b5a91c4142ef5124bbb9d
Kimsangwon-093 <akg9380@daum.net>
2025-11-20 17:13:23 +0900

    251120 meertagram login > service & repository,ORM(UserModel) set

--- server/app/controllers/auth.controller.js
+++ server/app/controllers/auth.controller.js
@@ -8,6 +8,7 @@ import { request } from "express";
 import { body } from "express-validator";
 import { SUCCESS } from "../../configs/responseCode.config.js";
 import { createBaseResponse } from "../utils/createBaseResponse.util.js";
+import authService from "../services/auth.service.js";
 
 // ----------------
 // -----public-----
@@ -21,9 +22,16 @@ import { createBaseResponse } from "../utils/createBaseResponse.util.js";
  * @returns
  */
 async function login(req, res, next) {
-  const body = req.body;
+  try {
+    const body = req.body; // 파라미터 획득
 
-  return res.status(SUCCESS.status).send(createBaseResponse(SUCCESS, body));
+    // 로그인 서비스 호출
+    const result = await authService.login(body);
+    
+    return res.status(SUCCESS.status).send(createBaseResponse(SUCCESS, result));
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 }
 // ----------
 // export
@@ -32,3 +40,9 @@ async function login(req, res, next) {
 export const authController = {
   login,
 };
+
+// 파라미터 유효성 검사
+// 이메일로 유저 검색 
+// 유저가 준 패스워드와 DB의 패스워드를 비교
+// 토큰생성
+// 토큰 및 정상 처리 응답 
