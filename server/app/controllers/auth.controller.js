131c8f724a446c30c7251687c2c6f3876f2b948b
Kimsangwon-093 <akg9380@daum.net>
2025-11-25 09:51:36 +0900

    251124 meertagram( Post Model udt )

--- server/app/controllers/auth.controller.js
+++ server/app/controllers/auth.controller.js
@@ -1,48 +1,39 @@
 /**
  * @file app/controllers/auth.controller.js
  * @description 인증 관련 컨트롤러
- * 251119 v1.0.0 Kim init
+ * 251119 v1.0.0 park init
  */
 
-import { request } from "express";
-import { body } from "express-validator";
 import { SUCCESS } from "../../configs/responseCode.config.js";
-import { createBaseResponse } from "../utils/createBaseResponse.util.js";
 import authService from "../services/auth.service.js";
+import { createBaseResponse } from "../utils/createBaseResponse.util.js";
 
 // ----------------
-// -----public-----
+// ---- public ----
 // ----------------
 /**
- * 
  * 로그인 컨트롤러 처리
- * @param {import("express").Request} req - 리퀘스트 객체
- * @param {import("express").Response} res - 리스폰스 객체
- * @param {import("express").NextFunction} next - next 객체
+ * @param {import("express").Request} req - Request 객체
+ * @param {import("express").Response} res - Response 객체
+ * @param {import("express").NextFunction} next - NextFunction 객체 
  * @returns
  */
 async function login(req, res, next) {
   try {
     const body = req.body; // 파라미터 획득
-
+    
     // 로그인 서비스 호출
     const result = await authService.login(body);
-    
+
     return res.status(SUCCESS.status).send(createBaseResponse(SUCCESS, result));
-  } catch (error) {
-    return res.status(500).send(error.message);
+  } catch(error) {
+    next(error);
   }
 }
-// ----------
-// export
-// ----------
 
-export const authController = {
+// --------------
+// export
+// --------------
+export default {
   login,
 };
-
-// 파라미터 유효성 검사
-// 이메일로 유저 검색 
-// 유저가 준 패스워드와 DB의 패스워드를 비교
-// 토큰생성
-// 토큰 및 정상 처리 응답 
--- server/configs/responseCode.config.js
+++ server/configs/responseCode.config.js
@@ -1,44 +1,44 @@
 /**
  * @file configs/responseCode.config.js
- * @description 서비스 전역 응답 코드 설정 모듈(각 API 응답시 참조되는 표준 응답 코드 정의)
- * 251119 v1.0.0 Kim init
+ * @description 서비스 전역 응답 코드 설정 모듈(각 API 응답 시 참조되는 표준 응답 코드 정의)
+ * 251119 v1.0.0 park init
  */
 
-// ---------------------------
+// ---------------------
 // type import
-// ---------------------------
+// ---------------------
 /**
  * @typedef {import('./responseCode.config.type.js').ResponseCodeConfig} ResponseCodeConfig
  */
 
-// ---------------------------
+// ---------------------
 /**
- * 정상 처리 응딥 코드 설정
- * @type {ResponseCodeConfig} 
+ * 정상 처리 응답 코드 설정
+ * @type {ResponseCodeConfig}
  */
 const SUCCESS = {
   code: '00',
   msg: 'NOMAL_CODE',
-  info:'정상처리',
+  info: '정상 처리',
   status: 200
-} // 상수로만 쓰기로 약속
-Object.freeze(SUCCESS); //이 객체는 외부에서 수정 못함
+}
+Object.freeze(SUCCESS);
 
 /**
- * 로그인 에러 응딥 코드 설정
- * @type {ResponseCodeConfig} 
+ * 로그인 에러 응답 코드 설정
+ * @type {ResponseCodeConfig}
  */
 const NOT_REGISTERD_ERROR = {
   code: 'E01',
-  msg: 'Unauhtorized Error',
-  info: '아이디나 비밀번호가 틀렸습니다',
+  msg: 'Unauthorized Error',
+  info: '아이디나 비밀번호가 틀렸습니다.',
   status: 400
 }
 Object.freeze(NOT_REGISTERD_ERROR);
 
 /**
- * 파라미터 에러 응딥 코드 설정
- * @type {ResponseCodeConfig} 
+ * 파라미터 에러 응답 코드 설정
+ * @type {ResponseCodeConfig}
  */
 const BAD_REQUEST_ERROR = {
   code: 'E21',
@@ -48,8 +48,34 @@ const BAD_REQUEST_ERROR = {
 }
 Object.freeze(BAD_REQUEST_ERROR);
 
+/**
+ * DB 에러 응답 코드 설정
+ * @type {ResponseCodeConfig}
+ */
+const DB_ERROR = {
+  code: 'E80',
+  msg: 'DB Error',
+  info: '서비스 제공 상태가 원활하지 않습니다.',
+  status: 500
+}
+Object.freeze(DB_ERROR);
+
+/**
+ * 시스템 에러 응답 코드 설정
+ * @type {ResponseCodeConfig}
+ */
+const SYSTEM_ERROR = {
+  code: 'E99',
+  msg: 'Application Error',
+  info: '서비스 제공 상태가 원활하지 않습니다.',
+  status: 500
+}
+Object.freeze(SYSTEM_ERROR);
+
 export {
   SUCCESS,
   NOT_REGISTERD_ERROR,
   BAD_REQUEST_ERROR,
+  DB_ERROR,
+  SYSTEM_ERROR,
 }
